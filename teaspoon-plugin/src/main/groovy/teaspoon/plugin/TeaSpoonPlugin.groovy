a37835abdb4b3d4fe9628e47fee02d6733b5ae57
Minkyu Ko <minkyuko@makeus.com>
2016-01-03 01:29:55 +0900

    init all necessary modules.
    annotations module, processor module, plugin module, and application
    module added.

---
+++ teaspoon-plugin/src/main/groovy/teaspoon/plugin/TeaSpoonPlugin.groovy
@@ -0,0 +1,48 @@
+package teaspoon.plugin
+
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryPlugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
+
+class TeaSpoonPlugin implements Plugin<Project> {
+
+    @Override void apply(Project project) {
+        def hasApp = project.plugins.withType(AppPlugin)
+        def hasLib = project.plugins.withType(LibraryPlugin)
+        if (!hasApp && !hasLib) {
+            throw new IllegalStateException("'android' or 'android-library' plugin required.")
+        }
+
+        final def variants
+        if (hasApp) {
+            variants = project.android.applicationVariants
+        } else {
+            variants = project.android.libraryVariants
+        }
+
+        project.dependencies {
+            compile project(':teaspoon-processor')
+            compile 'org.aspectj:aspectjrt:1.8.6'
+            compile project(':teaspoon-annotations')
+        }
+
+        variants.all { variant ->
+
+            JavaCompile javaCompile = variant.javaCompile
+            javaCompile.doLast {
+                String[] args = [
+                        "-showWeaveInfo",
+                        "-1.5",
+                        "-inpath", javaCompile.destinationDir.toString(),
+                        "-aspectpath", javaCompile.classpath.asPath,
+                        "-d", javaCompile.destinationDir.toString(),
+                        "-classpath", javaCompile.classpath.asPath,
+                        "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)
+                ]
+
+            }
+        }
+    }
+}
