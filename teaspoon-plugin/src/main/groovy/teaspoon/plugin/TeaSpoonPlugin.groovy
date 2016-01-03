8ab7b0f5de13af80f5c2831fe6eb80f44dbb6ca9
Minkyu Ko <minkyuko@makeus.com>
2016-01-03 16:14:23 +0900

    change version name to 0.2.1

--- teaspoon-plugin/src/main/groovy/teaspoon/plugin/TeaSpoonPlugin.groovy
+++ teaspoon-plugin/src/main/groovy/teaspoon/plugin/TeaSpoonPlugin.groovy
@@ -27,9 +27,9 @@ class TeaSpoonPlugin implements Plugin<Project> {
         }
 
         project.dependencies {
-            compile 'com.github.KoMinkyu.TeaSpoon:teaspoon-processor:0.1.1-alpha'
+            compile 'com.github.KoMinkyu.TeaSpoon:teaspoon-processor:0.2.1'
             compile 'org.aspectj:aspectjrt:1.8.6'
-            compile 'com.github.KoMinkyu.TeaSpoon:teaspoon-annotations:0.1.1-alpha'
+            compile 'com.github.KoMinkyu.TeaSpoon:teaspoon-annotations:0.2.1'
         }
 
         variants.all { variant ->
