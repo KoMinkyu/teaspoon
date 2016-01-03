bc86469ca9def88de6ea6f516a3d4fa91fead656
Minkyu Ko <minkyuko@makeus.com>
2016-01-03 14:21:12 +0900

    add @OnUi usage in teaspoon-example.

--- teaspoon-example/src/main/java/com/example/teaspoon/MainActivity.java
+++ teaspoon-example/src/main/java/com/example/teaspoon/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.teaspoon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import teaspoon.annotations.OnUi;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	TextView tvHello;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHello = (TextView) findViewById(R.id.main_tv_hello);
+
+        test_hideHelloTextView();
+    }
+
+    @OnUi private void test_hideHelloTextView() {
+    	tvHello.setVisibility(View.INVISIBLE);
     }
 }
