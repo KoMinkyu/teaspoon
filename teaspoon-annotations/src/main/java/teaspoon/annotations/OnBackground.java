b87867b3bd532ba7a0da379b763bde1e31640a40
Minkyu Ko <minkyuko@makeus.com>
2016-01-11 00:44:02 +0900

    Erase useless comment.

--- teaspoon-annotations/src/main/java/teaspoon/annotations/OnBackground.java
+++ teaspoon-annotations/src/main/java/teaspoon/annotations/OnBackground.java
@@ -5,9 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Created by makeus on 2015. 12. 30..
- */
 @Retention(RetentionPolicy.CLASS) @Target(ElementType.METHOD)
 public @interface OnBackground {
 }
