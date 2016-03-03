b248302f2f19121ce510496c25cbf2cdd99109ab
Minkyu Ko <minkyuko@makeus.com>
2016-03-03 16:25:26 +0900

    Changed `@OnUi` annotation's retention policy for annotation processing
    fix #6

--- teaspoon-annotations/src/main/java/teaspoon/annotations/OnUi.java
+++ teaspoon-annotations/src/main/java/teaspoon/annotations/OnUi.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.CLASS) @Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME) @Target(ElementType.METHOD)
 public @interface OnUi {
     int delay() default 0;
 }
