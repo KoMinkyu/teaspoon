2e8b7c3585137ff7ba7c61deb4a01614c5c83b2e
KoMinkyu <minkyuko411@gmail.com>
2016-06-01 14:42:13 +0900

    ready for next version.

--- teaspoon-plugin/src/main/groovy/teaspoon/plugin/TeaSpoonPlugin.groovy
+++ teaspoon-plugin/src/main/groovy/teaspoon/plugin/TeaSpoonPlugin.groovy
@@ -27,10 +27,10 @@ class TeaSpoonPlugin implements Plugin<Project> {
         }
 
         project.dependencies {
-            compile 'me.minkyu.teaspoon:teaspoon-processor:0.3.4'
+            compile 'me.minkyu.teaspoon:teaspoon-processor:0.4.0'
             compile 'org.aspectj:aspectjrt:1.8.8'
 
-            compile 'me.minkyu.teaspoon:teaspoon-annotations:0.3.4'
+            compile 'me.minkyu.teaspoon:teaspoon-annotations:0.4.0'
         }
 
         variants.all { variant ->
